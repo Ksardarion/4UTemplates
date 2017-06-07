c37f9ee92ad884d47a1c94ad84123a703e1d6b8f
Olecksandr <velsashok@gmail.com>
2017-06-07 20:26:01 +0300

    add plumber plugin to catch errors without stopping gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 let gulp = require('gulp');
+const plumber = require('gulp-plumber');
 
 let pug = require('gulp-pug');
 let sourcemaps = require('gulp-sourcemaps');
 let sass = require('gulp-sass');
 
+var onError = function (err) {  
+    gutil.beep();
+    console.log(err);
+};
 // let htmlbeautify = require('gulp-html-beautify');
 
 gulp.task('buildHTML', function buildHTML() {
   return gulp.src('./src/views/!(_)*.pug')
+  .pipe(plumber({
+    errorHandler: onError   
+   }))
   .pipe(pug({pretty: true}))
   // .pipe(htmlbeautify())
   .pipe(gulp.dest('./dist'));
